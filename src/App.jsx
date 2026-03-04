d72fdb4e0e8bed343a16bcfb7bcbc73000d5d5f9
Sayed <muhammadsaye741@gmail.com>
2026-03-05 00:15:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <button className="btn">Default</button>
+      <h1 className='font-bold'>Satup react</h1>
+    </>
+  )
+}
+
+export default App
